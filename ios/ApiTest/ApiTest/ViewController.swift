905a97e103ecec3b53d7d5d42aac0e6b5bd3f87a
yukimine <yuk.gt.imine@gmail.com>
2016-11-27 14:18:17 +0900

    OpenWeatherMapにリクエストを投げてデバッグ表示するところまで

--- ios/ApiTest/ApiTest/ViewController.swift
+++ ios/ApiTest/ApiTest/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendRequest(_ sender: AnyObject) {
+        let urlString = "http://api.openweathermap.org/data/2.5/forecast?units=metric&q=Tokyo&APPID="
+        let url = URL(string: urlString)!
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Content-type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = "GET"
+        
+        URLSession.shared.dataTask(with: request) {data, response, err in
+            if (err == nil) {
+                // 成功
+                print("success")
+                print(response)
+            } else {
+                // 失敗
+                print("fail")
+            }
+        }.resume()
+    }
 
 }
 
